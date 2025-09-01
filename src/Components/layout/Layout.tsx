380afe32d49d47c702a2072c008bc8973b3c9c16
Pratik Yesane <pratiky2000@gmail.com>
2025-09-02 04:02:25 +0530

    fix: standardize formatting and improve readability in Layout and Dialog components

--- src/Components/layout/Layout.tsx
+++ src/Components/layout/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <main>
+                <main className="p-2">
                     <SidebarTrigger />
                     {children}
                 </main>

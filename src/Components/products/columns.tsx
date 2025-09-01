1cc44024a8752b007a7602ed03b76aa59320c45e
Pratik Yesane <pratiky2000@gmail.com>
2025-09-02 04:00:38 +0530

    feat: implement DataTableColumnHeader component for sortable and filterable columns, and integrate EditProduct and DeleteProduct components in the action column

--- src/Components/products/columns.tsx
+++ src/Components/products/columns.tsx
@@ -1,11 +1,17 @@
 import type { Product } from '@/api/api';
 import { type ColumnDef } from '@tanstack/react-table';
+import EditProduct from './EditProduct';
+import DeleteProduct from './DeleteProduct';
+import { DataTableColumnHeader } from './coloumn-header';
 
 export const columns: ColumnDef<Product>[] = [
-    { accessorKey: 'title', header: 'Title' },
+    { accessorKey: 'title', header: 'Title', size: 350 },
+    { accessorKey: 'category', header: 'Category', size: 150 },
     {
         accessorKey: 'price',
-        header: 'Price',
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Price" />
+        ),
         cell: ({ row }) => {
             const price = parseFloat(row.getValue('price'));
             const formatted = new Intl.NumberFormat('en-US', {
@@ -14,7 +20,28 @@ export const columns: ColumnDef<Product>[] = [
             }).format(price);
             return <div>{formatted}</div>;
         },
+        size: 100,
     },
-    { accessorKey: 'category', header: 'Category' },
-    { accessorKey: 'stock', header: 'Stock' },
+    {
+        accessorKey: 'stock',
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Stock" />
+        ),
+        size: 80,
+    },
+    {
+        id: 'action',
+        enableHiding: false,
+        cell: ({ row }) => {
+            const product = row.original;
+
+            return (
+                <div className="flex items-center gap-3">
+                    <EditProduct product={product} />
+                    <DeleteProduct id={product.id} />
+                </div>
+            );
+        },
+        size: 100,
+    },
 ];

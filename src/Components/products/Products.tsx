cc45ba35469b349143e5f5e58ded2ae60a1286bb
Pratik Yesane <pratiky2000@gmail.com>
2025-09-02 18:43:10 +0530

    feat: add category filtering to product table and enhance layout with ScrollArea

--- src/Components/products/Products.tsx
+++ src/Components/products/Products.tsx
@@ -10,10 +10,15 @@ import {
     type ColumnFiltersState,
     type SortingState,
     getSortedRowModel,
+    getFacetedUniqueValues,
+    getFacetedRowModel,
+    getFacetedMinMaxValues,
 } from '@tanstack/react-table';
 import { ProductTablePagination } from './ProductTablePagination';
 import { Input } from '../ui/input';
 import AddProduct from './AddProduct';
+import CategoryFilter from './CategoryFilter';
+import { ScrollArea } from '../ui/scroll-area';
 
 const Products = () => {
     const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
@@ -30,6 +35,9 @@ const Products = () => {
         onColumnFiltersChange: setColumnFilters,
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
+        getFacetedRowModel: getFacetedRowModel(),
+        getFacetedUniqueValues: getFacetedUniqueValues(),
+        getFacetedMinMaxValues: getFacetedMinMaxValues(),
         state: {
             columnFilters,
             sorting,
@@ -41,29 +49,43 @@ const Products = () => {
         },
     });
 
+    const priceRange = table.getColumn('price')?.getFacetedMinMaxValues();
+    console.log(priceRange);
+
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error loading products</div>;
 
     return (
-        <div>
-            <div className="flex items-center justify-between py-4">
-                <Input
-                    placeholder="Search products..."
-                    value={
-                        (table
-                            .getColumn('title')
-                            ?.getFilterValue() as string) ?? ''
-                    }
-                    onChange={(e) =>
-                        table.getColumn('title')?.setFilterValue(e.target.value)
-                    }
-                    className="max-w-sm"
-                />
-                <AddProduct />
+        <div className="w-full h-fit py-2 px-6">
+            <div className="mt-4">
+                <h1 className="text-4xl font-semibold">Welcome User</h1>
             </div>
-
-            <ProductTable columns={columns} table={table} />
-            <ProductTablePagination table={table} />
+            <div className="h-fit lex flex-col items-center py-4">
+                <div className="flex items-center justify-between gap-2 w-full mb-2">
+                    <div className="flex gap-2">
+                        <Input
+                            placeholder="Search products..."
+                            value={
+                                (table
+                                    .getColumn('title')
+                                    ?.getFilterValue() as string) ?? ''
+                            }
+                            onChange={(e) =>
+                                table
+                                    .getColumn('title')
+                                    ?.setFilterValue(e.target.value)
+                            }
+                            className="max-w-sm"
+                        />
+                        <CategoryFilter column={table.getColumn('category')!} />
+                    </div>
+                    <AddProduct />
+                </div>
+                <ScrollArea className="h-[520px] w-full">
+                    <ProductTable columns={columns} table={table} />
+                </ScrollArea>
+                <ProductTablePagination table={table} />
+            </div>
         </div>
     );
 };
